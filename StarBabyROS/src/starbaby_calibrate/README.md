e2abf35e52ce95565b067fdaa2a39005bbae8501
Julien BAYLE <jbayle@gmail.com>
2018-04-19 00:30:44 +0200

    First implementation of IMU calibration

--- StarBabyROS/src/starbaby_calibrate/README.md
+++ StarBabyROS/src/starbaby_calibrate/README.md
@@ -1,12 +1,6 @@
 # Installation
 
-This package procides 3 services :
- * calibrate IMU : Put the robot in front of a wall (50 cm, nothin 1 meter around) and execute this service.
-   the robot will turn 10 times and provides IMU calibration data.
-   IMU node configuration could be ajusted using these.
-
- * calibrate Odometry : Put the robot in front of a wall (about 50 cm, nothing 2 meters around) and execute thie service.
-   the robot will move for about 1 minute and provides Odometru calibration data.
-   Odometry configuration counld be ajusted using these
-
- * PID test : validate PID value are suitables
+This package provides 3 services :
+ * calibrate IMU
+ * calibrate Odometry
+ * test PID values
--- StarBabyROS/src/starbaby_calibrate/scripts/calibrate/calibrate.py
+++ StarBabyROS/src/starbaby_calibrate/scripts/calibrate/calibrate.py
@@ -9,10 +9,24 @@ from starbaby_calibrate.msg import CalibrateAction
 from starbaby_calibrate.msg import CalibrateResult
 from sensor_msgs.msg        import Imu
 
+# The robot must be at 30 cm from a wall with nothing around (1 meter minimum)
+#
+# Phases
+#
+# IMU biais 
+#  - compute IMU biais when the robot is not moving
+#
+# IMU coef
+# - compute IMU coeficient to convert speed in degree per second
+# - use your pad to make the robot turn clockwise
+#
+# End :
+#  - action server returns the result message and log the calibration result 
+
+
 # File name is calibrate.py instead of starbaby_calibrate.py as ROS
-# is unable to import package msg if name is the same
+# is unable to import message from the package if the name is the same
 # Reference : https://answers.ros.org/question/173507/cannot-import-custom-service/
-# Took me one hour to understand this !
 class starbabyCalibrate:
 
     _feedback = CalibrateFeedback()
@@ -23,19 +37,39 @@ class starbabyCalibrate:
         self._as = actionlib.SimpleActionServer(self._action_name, CalibrateAction, execute_cb = self.execute_cb, auto_start = False)
         self._as.start()
 
-        rospy.loginfo("Calibration action %s ready " % self._action_name)
+        rospy.loginfo("Calibration action server started %s", self._action_name)
 
     def execute_cb(self, goal):
-        # imu
-        self.last_imu = []
-        self.average = 0
-        
-        # turn counter
-        self.start_angle = -1
-        self.turn_count = -1
-        self.angle_position = True
 
-        rospy.loginfo("Calibration waiting for inputs messages...")
+        # current phase name and completeness (from 0 to 100)
+        self.phase = "IMU biais"
+        self.phase_progress = 0
+
+        # imu measure memory to compute biais
+        self.imu_biais_data = []
+        self.imu_biais_max_iteration = 200
+
+        # imu computed biais on gz
+        self.imu_biais_gz = 0
+        
+	# imu rotation computed as an integral of gz over time
+	self.rotation = 0.0
+
+	# imu rotation memory every turn
+        self.imu_calib = []
+ 
+        # turn counter
+        self.previous_angle = 0
+        self.turn_count = 0
+        self.max_turn = 10
+        
+        # imu average angle error for a 360 degree turn
+        self.imu_angle_error = 0 
+
+        # start distance for odometry calibration
+        self.start_distance = -1
+     
+        rospy.loginfo("Waiting for inputs messages...")
 
         imu_sub = rospy.Subscriber('imu', Imu, self.execute_imu_cb)
         laser_angle_sub = rospy.Subscriber('scan_min_distance_angle', Float64, self.execute_min_distance_angle_cb)
@@ -44,39 +78,75 @@ class starbabyCalibrate:
         r = rospy.Rate(1)
 
         rospy.loginfo("Starting calibration...")
-        while not rospy.is_shutdown():
+        while not (rospy.is_shutdown() or self.phase == "End"):
            if self._as.is_preempt_requested():
              rospy.loginfo('%s: Preempted' % self._action_name)
              self._as.set_preempted()
              break
-           rospy.loginfo("Calibration processing...")
+           rospy.loginfo("Calibration phase : %s (%f %%)", self.phase, 100*self.phase_progress)
            r.sleep()
 
-        rospy.loginfo("Calibration stopped, cleaning...")
+        rospy.loginfo("Add to the IMU biais: %f" % self.imu_biais_gz)
+        rospy.loginfo("Average rotation error %f",self.imu_angle_error)
+        rospy.loginfo("Multiply the to_degree_per_second parameter %f",(360/(360+self.imu_angle_error)))
+ 
+        rospy.loginfo("Calibration stopped or finished, cleaning...")
 
         imu_sub.unregister()
         laser_angle_sub.unregister()
         laser_distance_sub.unregister()
         
-        rospy.loginfo("Calibration done")
+        rospy.loginfo("Calibration process done")
 
     def execute_imu_cb(self, data):
-        now = rospy.Time.now()
-        if self.average == 0 and len(last_imu < 100):
-          self.last_imu.append(data.angular_velocity.z)
-          rospy.loginfo("IMU gz : %f" % data.angular_velocity.z-self.average)
-	else:
-          self.average = sum(last_imu)/float(len(last_imu))
 
+        now = rospy.get_time()
+        if self.phase == "IMU biais" and len(self.imu_biais_data) < self.imu_biais_max_iteration:
+          self.imu_biais_data.append(data.angular_velocity.z)
+          self.phase_progress = len(self.imu_biais_data) / float(self.imu_biais_max_iteration)
+	elif self.phase == "IMU biais":
+          self.imu_biais_gz = sum(self.imu_biais_data)/float(len(self.imu_biais_data))
+          self.phase = "IMU coef"
+          self.phase_progress = 0
+        elif self.phase == "IMU coef" and self.turn_count > 0 :
+          self.rotation += (now - self.lasttime)*(data.angular_velocity.z-self.imu_biais_gz)
+        self.lasttime = now
+          
     def execute_min_distance_cb(self, data):
-        rospy.loginfo("Distance : %f" % data.data)
+        if self.start_distance < 0:
+           self.start_distance = data.data
+           rospy.loginfo("Calibration / Start distance : %f" % data.data)
 
     def execute_min_distance_angle_cb(self, data):
-        if self.start_angle < 0:
-           selft.start_angle = data.data
-           rospy.loginfo("Start angle : %f" % data.data)
+        angle = data.data
 
-        if 
+        # Detect turn when angle pass over 0 degree ( turns such as LIDAR angle increase   
+        if self.previous_angle > angle and angle > 180:
+          rospy.logwarn("Robot must turn clockwise, please restart calibration process")
+
+        if self.phase == "IMU coef" and self.previous_angle > 180 and angle < 100:
+          self.turn_count += 1
+          self.phase_progress = self.turn_count / float(self.max_turn)   
+          self.imu_calib.append((self.turn_count, angle, self.rotation))
+ 
+        if self.phase == "IMU coef" and self.turn_count >= self.max_turn:
+          last_imu_angle = last_lidar_angle = 0
+          delta_rots = []
+          for turn, lidar_angle, imu_angle in self.imu_calib:
+            lidar_rot = -360 -lidar_angle + last_lidar_angle # ROS see positive angle when turning counter-clockwise
+            imu_rot = imu_angle - last_imu_angle 
+            delta_rot = lidar_rot - imu_rot 
+            rospy.loginfo("%f - Rotation seen by the LIDAR %f degrees and by the IMU %f degrees (Delta %f)", \
+              turn, lidar_rot, imu_rot, delta_rot)
+            last_imu_angle = imu_angle
+            last_lidar_angle = lidar_angle
+            if turn > 2: # First two turns are ignored
+              delta_rots.append(delta_rot)
+          self.imu_angle_error = sum(delta_rots)/float(len(delta_rots))
+          self.phase = "End"
+         
+        self.previous_angle = angle
+
 if __name__ == '__main__':
     rospy.init_node('starbaby_calibrate')
     starbaby_calibrate = starbabyCalibrate(rospy.get_name())
