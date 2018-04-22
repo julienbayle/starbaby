7a6cbd01330947983a5739e9e6228b76d622f518
Julien BAYLE <jbayle@gmail.com>
2018-04-22 21:44:52 +0200

    Allow robot to work without network

--- StarBabyService/start.sh
+++ StarBabyService/start.sh
@@ -3,6 +3,10 @@
 BASE=$(dirname "$0")
 IP=`ip addr | grep 'inet .* wlan0' | awk '{print $2}' | cut -f1 -d'/'`
 
+if [ -z "$IP" ]; then 
+	IP="127.0.0.1"
+fi
+
 source $BASE/../StarBabyROS/devel/setup.bash
 export ROS_IP=$IP
 export ROS_MASTER=http://$IP:11311
