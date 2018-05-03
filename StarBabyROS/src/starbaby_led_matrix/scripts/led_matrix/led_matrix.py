71bd4202421cbc5f2e189c1878643f2f39877faa
Julien BAYLE <jbayle@gmail.com>
2018-05-04 01:42:31 +0200

    First version of the robot eye node

--- StarBabyROS/src/starbaby_led_matrix/scripts/led_matrix/led_matrix.py
+++ StarBabyROS/src/starbaby_led_matrix/scripts/led_matrix/led_matrix.py
@@ -1,78 +1,104 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017-18 Richard Hull and contributors
-# See LICENSE.rst for details.
 
-import re
-import time
-import argparse
+"""
+Subscribes to robot eye messages and displays it
 
+By default, when no message has to be displayed, 
+robot eyes are animated with fun behaviours
+
+"""
+
+import rospy
+import roslib
+roslib.load_manifest('starbaby_led_matrix')
+import os.path
+from PIL import Image, ImageSequence
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
-from luma.core.render import canvas
-from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from luma.core.sprite_system import framerate_regulator
+from starbaby_led_matrix.msg import Eye
+from luma.core.render import canvas
 
+class RobotEye:
+    def __init__(self, img_path):
+        rospy.init_node("starbaby_led_matrix")
+        self.repeat=0
+        self.img_path = img_path
+        rospy.Subscriber("/starbaby/eye", Eye, self.execute_msg_cb)
+        serial = spi(port=0, device=0, gpio=noop())
+        self.device = max7219(serial, width=32, height=8)
+        self.device.contrast(0x10)
+        rospy.loginfo("Robot eye LCD Matrix ready")
+        self.device.preprocess = self.preprocess
 
-def demo(n, block_orientation, rotate):
-    # create matrix device
-    serial = spi(port=0, device=0, gpio=noop())
-    #device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation, rotate=rotate or 0)
-    device = max7219(serial, width=32, height=8, block_orientation=-90, rotate=2)
-    print("Created device")
-    device.contrast(0x10)
+    def preprocess(self, image):
+        """
+        Make a -90° flip for each 8x8 LCD segment and a full rotate of the image
+        This is done here as the luma_core has a bug when doing such composition
+        """
+        image = image.rotate(-180)
+        image = image.copy()
+        for x in range(0, 32, 8):
+            box = (x, 0, x + 8, 8)
+            rotated_block = image.crop(box).rotate(-90)
+            image.paste(rotated_block, box)
+        return image
+
+    def execute_msg_cb(self, msg):
+        # If text message is the name of an existing GIF then
+        # display the animated GIF
+        self.text = msg.text
+        img_path = os.path.join(self.img_path, self.text)
+        if os.path.isfile(img_path) :
+            self.image = Image.open(img_path)
+            self.text = ""
+
+        self.repeat = msg.repeat or 1
+        self.fps = msg.fps or 10
+        rospy.loginfo("Eye message received %s, %d, %d", self.text, self.repeat, self.fps)
+
+    def spin(self):
     
-    with canvas(device) as draw:
-        for i in range(33):
-            for j in range(9):
-                draw.point((i,  j), fill="white")
-    time.sleep(1)
+        while not rospy.is_shutdown():
+            
+            # A specific message should be displayed
+            if self.repeat:
 
-    # start demo
-    msg = "MAX7219 LED Matrix Demo"
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(CP437_FONT))
-    time.sleep(1)
+                # Display text message
+                if self.text:
+                    show_message(self.device, self.text, fill="white", font=proportional(CP437_FONT), scroll_delay=1/float(self.fps))    
 
-    msg = "Slow scrolling: The quick brown fox jumps over the lazy dog"
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
+                # Display animated GIF
+                else:
+                    regulator = framerate_regulator(fps=self.fps)
+                    for frame in ImageSequence.Iterator(self.image):
+                        with regulator:
+                            #background = Image.new("RGB", self.device.size, "white")
+                            #background.paste(frame.resize((32,8), resample=Image.LANCZOS), (0,0))
+                            #background.paste(frame)
+                            b = frame.load()
+                            #m =  [""] * 8
+                            with canvas(self.device) as draw:
+                                for i in range(32):
+                                    for j in range(8):
+                                        if b[i,j]:
+                                            draw.point((i, j), fill="black")
+                                        else:
+                                            draw.point((i, j), fill="white")
+                            #self.device.display(frame.convert(self.device.mode))
+                            #self.device.display(background.convert(self.device.mode))
+                
+                self.repeat -= 1
 
-    print("Vertical scrolling")
-    words = [
-        "Victor", "Echo", "Romeo", "Tango", "India", "Charlie", "Alpha",
-        "Lima", " ", "Sierra", "Charlie", "Romeo", "Oscar", "Lima", "Lima",
-        "India", "November", "Golf", " "
-    ]
+            # Display default robot eye behaviour
+            else:
+                show_message(self.device, "WWMM", fill="white", font=proportional(CP437_FONT))
 
-    virtual = viewport(device, width=device.width, height=len(words) * 8)
-    with canvas(virtual) as draw:
-        for i, word in enumerate(words):
-            text(draw, (0, i * 8), word, fill="white", font=proportional(CP437_FONT))
-
-    for i in range(virtual.height - device.height):
-        virtual.set_position((0, i))
-        time.sleep(0.05)
-
-    msg = "Brightness"
-    print(msg)
-    show_message(device, msg, fill="white")
-
-    time.sleep(1)
-    with canvas(device) as draw:
-        text(draw, (0, 0), "A", fill="white")
-
-    time.sleep(1)
-    for _ in range(5):
-        for intensity in range(16):
-            device.contrast(intensity * 16)
-            time.sleep(0.1)
-
-    time.sleep(1)
 
 if __name__ == "__main__":
-    try:
-        demo(4, -90, 0)
-    except KeyboardInterrupt:
-        pass
+    img_path = os.path.join(os.path.dirname(__file__), '..', '..', 'images')
+    robot_eye = RobotEye(img_path)
+    robot_eye.spin()
